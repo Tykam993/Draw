fe420237e7b569dea94204deae515ab02df99874
Tyler Kaminsky <tylerkaminsky@gmail.com>
2017-01-18 11:14:38 -0500

    Starting branch with current work

---
+++ Assets/movement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+    [SerializeField]
+    private string direction;
+
+	public GameObject time_keeper;
+	private bool time_expired;
+	[SerializeField]
+	private int player_number;
+	[SerializeField]
+	private string key;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    void walk(string dir)
+	{
+        if (direction == "left")
+        {   
+            Vector3 left = new Vector3(-.05f, 0, 0);
+            transform.position += left;
+        }
+
+        if (direction == "right")
+        {
+            Vector3 right = new Vector3(.05f, 0, 0);
+            transform.position += right;
+        }
+    }
+
+	void fire_weapon() {
+			time_keeper.GetComponent<time> ().shots.Add (player_number);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		time_expired = time_keeper.GetComponent<time>().expired;
+		if (!time_expired) {
+			walk (direction);
+		}
+		if (Input.GetKeyDown (key)) {
+			fire_weapon ();
+			Debug.Log("YO");
+		}
+	}
+}
---
+++ Assets/time.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class time : MonoBehaviour {
+	private DateTime start = DateTime.Now;
+	public List<int> shots = new List<int>();
+	[SerializeField]
+	public bool expired = false;
+	[SerializeField]
+	private int[] score = new int[2];
+
+	void countdown (DateTime time_start)
+	{	
+		DateTime now = DateTime.Now;
+		if ((now.Second - time_start.Second) >= 3 && !expired) {
+			expired = true;
+		}
+    }
+
+	// Use this for initialization
+	void Start () {
+		score[0] = 0;
+		score[1] = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		countdown (start);
+		if (shots.Count > 0) {
+			score[shots[0]] += 1;
+			Debug.Log (score);
+			shots = new List<int> ();
+		}
+	}
+}
